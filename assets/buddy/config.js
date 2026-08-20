a7dfa9ba0b55dcd5cdb0336e595d697c530d9a7d
Alejandro Sosa Briceño <asosab@gmail.com>
2026-08-20 12:55:28 -0400

    debug: false

--- assets/buddy/config.js
+++ assets/buddy/config.js
@@ -6,9 +6,9 @@
 window.BuddyConfig = window.BuddyConfig || {};
 window.BuddyConfig = Object.assign({
   // Debug global: todos los módulos Buddy deben usar esta bandera para sus trazas.
-  debug: true,
+  debug: false,
   // Compatibilidad con módulos existentes que todavía consultan debugMode.
-  debugMode: true,
+  debugMode: false,
   app: {
     siteId: 'arbat',
     email:  'arbat.archery@gmail.com'
