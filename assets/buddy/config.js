6ac23946098316c71c318ecc6ff536fe2cceebb2
Alejandro Sosa Briceño <asosab@gmail.com>
2026-08-22 13:50:18 -0400

    Update config.js

--- assets/buddy/config.js
+++ assets/buddy/config.js
@@ -6,7 +6,7 @@
 window.BuddyConfig = window.BuddyConfig || {};
 window.BuddyConfig = Object.assign({
   // Debug global: todos los módulos Buddy deben usar esta bandera para sus trazas.
-  debug: false,
+  debug: true,
   // Compatibilidad con módulos existentes que todavía consultan debugMode.
   debugMode: false,
   app: {
